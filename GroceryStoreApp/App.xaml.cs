c053f719ee5b53b3cc7f05603d641b504fe35b25
Wandile Gwebu <wandilegwebu8@gmail.com>
2023-12-16 20:26:13 +0200

    Add project files.

---
+++ GroceryStoreApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GroceryStoreApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GroceryStoreApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GroceryStoreApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
